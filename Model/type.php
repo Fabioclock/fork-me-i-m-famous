f40490c9f164f80825aa5b06db711515688f1b8e
Lacouleur <damien.voindrot@gmail.com>
2019-07-29 17:59:35 +0200

    implementation des models

---
+++ Model/type.php
@@ -0,0 +1,86 @@
+<?php
+
+
+class Type
+{
+
+    private $id; 
+    private $name;
+    private $footer_order;
+    private $created_at;
+    private $updated_at;
+    
+    
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of footer_order
+     */ 
+    public function getFooter_order()
+    {
+        return $this->footer_order;
+    }
+
+    /**
+     * Set the value of footer_order
+     *
+     * @return  self
+     */ 
+    public function setFooter_order($footer_order)
+    {
+        $this->footer_order = $footer_order;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created_at
+     */ 
+    public function getCreated_at()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Get the value of updated_at
+     */ 
+    public function getUpdated_at()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set the value of updated_at
+     *
+     * @return  self
+     */ 
+    public function setUpdated_at($updated_at)
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
